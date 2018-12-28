9c7dafea85164b83aaca64effa093a1a863d356b
Nathan Boyd <nathan.boyd.481@my.csun.edu>
2018-12-28 12:58:56 -0800

    chkpt.
    Still trying to figure out how to connect to the follow trajectory message and action topics

--- src/leg_joint_trajectory_action_server.cc
+++ src/leg_joint_trajectory_action_server.cc
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <vector>
 #include <actionlib/server/action_server.h>
 #include <trajectory_msgs/JointTrajectory.h>
 #include <ardent_controllers_msgs/JointTrajectoryAction.h>
@@ -6,10 +7,10 @@
 
 #define GOAL_THRESHOLD = 0.1;
 
-class JointTrajectoryExecutor
+class JointTrajectoryExecuter
 {
     private:
-        typedef actionlib::ActionServer<ardent_controllers_msgs::JointTrajectoryAction> JTAS
+        typedef actionlib::ActionServer<ardent_controllers_msgs::JointTrajectoryAction> JTAS;
         typedef JTAS::GoalHandle GoalHandle;
     public:
         ros::NodeHandle node;
@@ -29,45 +30,75 @@ class JointTrajectoryExecutor
         double goal_time_constraint;
         double stopped_velocity_tolerance;
 
-        pr2_controllers_msgs::JointTrajectoryControllerStateConstPtr last_controller_state;
+        ardent_controllers_msgs::JointTrajectoryControllerStateConstPtr last_controller_state;
 
-        JointTrajectoryExecutor(ros::NodeHandle &nh_) :  
+        std::string leg_id;
+
+        JointTrajectoryExecuter(std::string leg_id_, ros::NodeHandle &nh_) :  
             node(nh_), 
-            action_server(node,"joint_trajectory_action",
-                            boost::bind(&JointTrajectoryExecutor::goalCallback, this, _1),
-                            boost::bind(&JointTrajectoryExecutor::cancelCallback, this, _1),
+            action_server(node,"joint_trajectory_action_server",
+                            boost::bind(&JointTrajectoryExecuter::goalCallback, this, _1),
+                            boost::bind(&JointTrajectoryExecuter::cancelCallback, this, _1),
                             false),
             has_active_goal(false)
         {
             using namespace XmlRpc;
-            ros::NodeHandle pn("~");
-
+            leg_id = leg_id_;
+            ros::NodeHandle pn("leg_"+leg_id+"_traj_controller/");
+            // resolves "~/gripper_action" to "/grasping_points_node/gripper_action"
             // read joints from the robot xml
-            XmlRpc::XmlRpcValue joint_names;
-            if(!pn.getParam("joints", joint_names))
+            XmlRpc::XmlRpcValue joint_names_;
+            if(!pn.getParam("joints", joint_names_))
             {
                 ROS_FATAL("No joints given. (namespace: %s)", pn.getNamespace().c_str());
                 exit(1);
             }
-            if(joint_names.getType() != XmlRpc::XmlRpcValue::TypeArray)
+            if(joint_names_.getType() != XmlRpc::XmlRpcValue::TypeArray)
             {
                 ROS_FATAL("Malformed joint specification.  (namespace: %s)", pn.getNamespace().c_str());
                 exit(1);
             }
-            for (int i = 0; i < joint_names.size(); ++i)
+            for (size_t i = 0; i < joint_names_.size(); ++i)
             {
-                XmlRpcValue &name_value = joint_names[i];
-            if(name_value.getType() != XmlRpcValue::TypeString)
-            {
-                ROS_FATAL("Array of joint names should contain all strings.  (namespace: %s)",
-                        pn.getNamespace().c_str());
-                exit(1);
+                XmlRpcValue &name_value = joint_names_[i];
+                if(name_value.getType() != XmlRpcValue::TypeString)
+                {
+                    ROS_FATAL("Array of joint names should contain all strings.  (namespace: %s)", pn.getNamespace().c_str());
+                    exit(1);
+                }
+                else
+                {
+                    ROS_INFO("Added %s %s to the list of joints", pn.getNamespace().c_str(),(std::string)name_value);
+                }
+                joint_names.push_back((std::string)name_value);
             }
+            pn.param("constraints/goal_time", goal_time_constraint, 0.0);
+            for(size_t i =0; i < joint_names.size();++i)
+            {
+                std::string ns = std::string("constraints/")+joint_names[i];
+                double goal, traj;
+                pn.param(ns+"/goal",goal,-1.0);
+                pn.param(ns+"/trajectory",traj,-1.0);
+                goal_constraints[joint_names[i]] = traj;
+            }
+            pn.param("constaints/stopped_velocity_tolerance",stopped_velocity_tolerance, 0.01);
 
-            joint_names_.push_back((std::string)name_value);
-            }
+            pub_controller_command = node.advertise<trajectory_msgs::JointTrajectory>("command",1);
+            sub_controller_state = node.subscribe("state", 1, &JointTrajectoryExecuter::controllerStateCallback, this);
         }
 
+        ~JointTrajectoryExecuter()
+        {
+            pub_controller_command.shutdown();
+            sub_controller_state.shutdown();
+            //watchdog_timer.stop();
+        }
+
+        void watchdog(const ros::TimerEvent &e)
+        {
+            ros::Time now = ros::Time::now();
+
+        }
         void goalCallback(GoalHandle gh_)
         {
 
@@ -79,4 +110,18 @@ class JointTrajectoryExecutor
         }
 
         void controllerStateCallback(const ardent_controllers_msgs::JointTrajectoryControllerStateConstPtr &msg)
+        {
+
+        }
+};
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "joint_trajectory_action_node");
+    ros::NodeHandle node;//("~");
+    JointTrajectoryExecuter jte("rf",node);
+
+    ros::spin();
+
+    return 0;
 }
